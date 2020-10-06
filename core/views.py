24da96dc6c0d6856d684c5d605fbf408e152dd92
Fahim Ahmed <recentfahim13@gmail.com>
2020-10-06 16:17:42 +0600

    resolved api issue and post it to dana apps

--- core/views.py
+++ core/views.py
@@ -4,20 +4,32 @@ from rest_framework.response import Response
 from .helpers import bank_sms_parsing
 from rest_framework import status
 import json
+import requests
 
 
 class SMSParsing(APIView):
     def post(self, request, *args, **kwargs):
         data = request.data
         sms = data.get('data')
-        sms_string = json.dumps(sms)
-        print(sms_string)
-        parsed_data = bank_sms_parsing.parse_sms_data(data=sms_string)
-
-        context = {
-            'message': 'Parsed SMS',
-            'data': parsed_data,
-            'success': True
-        }
-
-        return Response(context, status=status.HTTP_200_OK)
+        if sms:
+            parsed_data = bank_sms_parsing.parse_sms_data(data=sms)
+            context = {
+                'message': 'Parsed SMS',
+                'data': parsed_data,
+                'success': True
+            }
+            header = {
+                'Content-type': 'application/json'
+            }
+            post_data = requests.post('http://softcell.yearstech.com/demo/dana/api/sms/parse',
+                                      data=parsed_data,
+                                      headers=header
+                                      )
+            return Response(context, status=status.HTTP_200_OK)
+        else:
+            context = {
+                'message': 'No Content',
+                'data': None,
+                'success': False
+            }
+            return Response(context, status=status.HTTP_204_NO_CONTENT)
